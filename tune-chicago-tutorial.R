987c93cc7b04f2208496e76f40249baca3f8d62d
Tom <wellurban@gmail.com>
2020-02-16 21:24:21 +1300

    Try to add other tidymodels features to workflow example

--- tune-chicago-tutorial.R
+++ tune-chicago-tutorial.R
@@ -97,23 +97,42 @@ set.seed(2132) # or earlier
 time_resamples <- rolling_origin(this_dataset,
   initial = 364 * 15, assess = 7 * 4, skip = 7 * 4, cumulative = FALSE)
 
-# 4. define tuning (usinf defaults?) then fit all versions for tuning
+# 4. define tuning (using defaults?) then fit all versions for tuning
+# parallelise if required
 grid_results <- tune_grid(this_recipe,
                           model = this_model,
                           resamples = time_resamples,
-                          grid = 20)
+                          grid = 20,
+                          control = control_grid(verbose = FALSE, save_pred = TRUE))
 
+# 4.5 evaluate tuning results
 grid_metrics <- collect_metrics(grid_results) # can then do custom vis
+grid_predictions <- collect_predictions(grid_results) # can then do custom vis for residual analysis
 grid_results %>% autoplot(metric = "rmse") # marginal plots: not recommended
 
 show_best(grid_results, metric = "rmse", maximize = FALSE)
 good_values <- select_best(grid_results, metric = "rmse", maximize = FALSE) # or manually select
 
 # 5. finalise recipe and model
-new_rec <- finalize_recipe(this_recipe, good_values)
-new_mod <- finalize_model(this_model, good_values)
+# new_rec <- finalize_recipe(this_recipe, good_values) # or prep()?
+new_recipe <- finalize_recipe(this_recipe, good_values) %>% prep()
+new_model <- finalize_model(this_model, good_values)
 
-# 6. evaluate on test set
+# 6. fit on entire training set and evaluate on test set
 
+final_fit <- new_model %>% 
+  fit(ridership ~ ., data = juice(new_recipe))
 
-# 7. predict or infer, as required
+test_pred <- final_fit %>%
+  predict(juice(new_recipe)) %>%
+  bind_cols(juice(new_recipe))
+# don't forget any transformations of predicted value
+
+perf_metrics <- metric_set(rmse, rsq, ccc)
+test_pred  %>%
+  perf_metrics(truth = ridership, estimate = .pred) # rmse estimate seems too low
+
+# 7. predict on new data, and/or inspect variable importance
+
+library(vip) # depends on model type
+

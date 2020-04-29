a4552bf44033bb246a2d357babdc1ad3c771b910
Tom <wellurban@gmail.com>
2020-04-29 15:58:23 +1200

    Do linear models & NNs; adapt pdp code for purrr

--- variable-importance-vignette.R
+++ variable-importance-vignette.R
@@ -63,3 +63,63 @@ bst %>%
   vip(num_features = 5, geom = "point", horizontal = FALSE, 
       aesthetics = list(color = "red", shape = 17, size = 4)) +
   theme_light()
+
+
+# linear models and MARS -----------------------------------------------------------
+
+linmod <- lm(y ~ .^2, data = trn)
+backward <- stats::step(linmod, direction = "backward", trace = 0) # recipes::step() conflicts with stats::step()
+
+vi(backward)
+
+backward %>% 
+  vip(num_features = length(coef(backward)),
+      geom = "point", horizontal = FALSE, mapping = aes_string(colour = "Sign"))
+
+backward %>% 
+  vi() %>% 
+  separate(Variable, into = c("var1", "var2"), sep = ":", fill = "right") %>% 
+  mutate(var2 = coalesce(var2, var1)) %>% 
+  ggplot() +
+  geom_point(aes(x = var1, y = var2, size = Importance, colour = Sign)) +
+  labs(x = "", y = "", title = "") +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank())
+
+library(earth)  
+
+mars <- earth(y ~ ., data = trn, degree = 2, pmethod = "exhaustive")
+vi(mars)
+vip(mars)
+
+
+# nn ----------------------------------------------------------------------
+
+library(nnet)
+set.seed(0803)
+nn <- nnet(y ~ ., data = trn, size = 7, decay = 0.1, linout = TRUE)
+grid.arrange(
+  vip(nn),
+  vip(nn, type = "garson"),
+  nrow = 1
+)
+
+methods(vi_model) %>% as.character() %>% stringr::str_remove(., "^vi_model\\.")
+
+
+# model-agnostic scores ---------------------------------------------------
+
+library(pdp)
+
+pp <- ppr(y ~ ., data = trn, nterms = 11)
+
+features <- paste0("x.", 1:10)
+
+pdps <- features %>% 
+  map(~ partial(pp, pred.var = .x) %>%
+               autoplot() +
+               ylim(range(trn$y)) +
+               theme_light()
+      )
+  
+grid.arrange(grobs = pdps, ncol = 5)

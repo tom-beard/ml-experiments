029095268e3dfe3bf9b07bbfd4b4e3b1499cea84
Tom <wellurban@gmail.com>
2020-05-16 16:32:43 +1200

    Examine variable importance and interactions

--- interpreting-xgboost.R
+++ interpreting-xgboost.R
@@ -42,5 +42,30 @@ ames_xgb <- xgboost::xgboost(
   eta = 0.1, gamma = 0
 )
 
-# Variable importance plot
+
+# variable importance -----------------------------------------------------
+
 vip(ames_xgb, num_features = 10)  # 10 is the default
+
+# c-ICE curves and PDPs for Overall_Qual and Gr_Liv_Area
+x <- data.matrix(subset(ames, select = -Sale_Price))  # training features
+p1 <- partial(ames_xgb, pred.var = "Overall_Qual",
+              ice = TRUE, center = TRUE, 
+              plot = TRUE, rug = TRUE, alpha = 0.07,
+              plot.engine = "ggplot2", 
+              train = x)
+p2 <- partial(ames_xgb, pred.var = "Gr_Liv_Area",
+              ice = TRUE, center = TRUE, 
+              plot = TRUE, rug = TRUE, alpha = 0.07,
+              plot.engine = "ggplot2",
+              train = x)
+# look for heterogeneity in ICE curves, suggesting possible interactions
+
+p3 <- partial(ames_xgb, pred.var = c("Overall_Qual", "Gr_Liv_Area"),
+              plot = TRUE, chull = TRUE,
+              plot.engine = "ggplot2",
+              train = x)
+
+grid.arrange(p1, p2, p3, ncol = 3)
+
+# to do: repeat using tidymodels!
